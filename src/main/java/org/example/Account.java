5f054a5a7ca5710aa651a36a2cfefa07f04be412
Jarek <jarek.malesza@gmail.com>
2022-11-07 18:24:33 +0100

    Rozpisanie logowania do aplikacji banku.

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Account {
+    private String name;
+    private String lastName;
+    private String pesel;
+    private String login;
+    private String acconutNumber;
+    private double balance;
+    private int pin;
+
+
+    Acconut(String name, String lastName, String pesel, String login, String acconutNumber, double balance, int pin){
+        this.name=name;
+        this.lastName=lastName;
+        this.pesel=pesel;
+        this.login=login;
+        this.acconutNumber=acconutNumber;
+        this.balance=balance;
+        this.pin=pin
+    }
+
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Account account1=new Account("Adam", "Adamek", "123456789" "AdAd","987654321", 1000000, 1234);
+        Scanner sc=new Scanner(System.in)
+
+
+        System.out.println("Witamy w naszym banku");
+        Scanner sc=new Scanner(System.in)
+        System.out.println("Uzytkowniku wpisz login" );
+        String login=sc.next();
+        System.out.println("podaj kod PIN");
+        int pin=sc.nextInt();
+
+    }
+
+}
